775ac331454137313fdb6fe546dd18d85150a549
Delirios <lushchevskiyd@gmail.com>
2020-12-19 00:53:13 +0200

    Added Ocelot API

--- TaxiService.APIGateway/Startup.cs
+++ TaxiService.APIGateway/Startup.cs
@@ -10,8 +10,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
-namespace TaxiService.Identity
+namespace TaxiService.APIGateway
 {
     public class Startup
     {
@@ -25,28 +27,22 @@ namespace TaxiService.Identity
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllers();
+            services.AddOcelot();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
-
             app.UseRouting();
-
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+            await app.UseOcelot();
         }
     }
 }
