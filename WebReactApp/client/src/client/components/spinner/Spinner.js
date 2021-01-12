40256e777f2502d4d9f7310ff2929415cc5de9de
Delirios <lushchevskiyd@gmail.com>
2021-01-12 21:56:27 +0200

    Created spinner component

--- WebReactApp/client/src/client/components/spinner/Spinner.js
+++ WebReactApp/client/src/client/components/spinner/Spinner.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { RotateSpinner } from "react-spinners-kit";
 
-import "./Spinner.css"
+import "./Spinner.css";
 
 class Spinner extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loading: true,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+    };
+  }
 
-    render() {
-        const { loading } = this.state;
-        return <div className="spinner-container"><RotateSpinner size={150} color="#686769" loading={loading} /></div>;
-    }
+  render() {
+    const { loading } = this.state;
+    return (
+      <div className="spinner-container">
+        <RotateSpinner size={150} color="#686769" loading={loading} />
+      </div>
+    );
+  }
 }
 
-export default Spinner
+export default Spinner;
